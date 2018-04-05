e0c69fe978b8fc888ec5d33d36dde4833dc3f7de
Monica Björk <m.bjork95@gmail.com>
2018-04-05 20:17:01 +0200

    Add project files.

---
+++ Programmering1_7.3.4-5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_7._3._4_5 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Vad är tempraturen i Östersund? ");
+            String östersundString = Console.ReadLine();
+            Double östersundTemp = Convert.ToDouble(östersundString);
+
+            Console.Write("Vad är tempraturen i Åmål? ");
+            String åmålString = Console.ReadLine();
+            Double åmålTemp = Convert.ToDouble(åmålString);
+
+            if (åmålTemp > östersundTemp) {
+                Console.WriteLine("Det är kallast i Östersund!");
+            }
+            else if (åmålTemp == östersundTemp) {
+                Console.WriteLine("Det är lika kallt!");
+            }
+            else {
+                Console.WriteLine("Det är kallast i Åmål!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
